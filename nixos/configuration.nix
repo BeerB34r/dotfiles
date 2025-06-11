3e6d50baa7700960e367d80849be68384b10e0b9
Mats de Beer <matsdebeerk@gmail.com>
2025-06-11 12:11:30 +0200

    added nixos to dotfile config

---
+++ nixos/configuration.nix
@@ -0,0 +1,74 @@
+{ config, pkgs, ... }:
+
+{
+	imports = 
+	[
+		/etc/nixos/hardware-configuration.nix # hardware scan
+		./the-goblin.nix # main user config
+	]
+
+# Bootloader
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+	
+# Various system configurations
+	networking.hostName = "jason";
+	networking.networkmanager.enable = true;
+	time.timeZone = "Europe/Amsterdam";
+	i18n.defaultLocale = "en_US.UTF-8";
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "nl_NL.UTF-8";
+		LC_IDENTIFICATION = "nl_NL.UTF-8";
+		LC_MEASUREMENT = "nl_NL.UTF-8";
+		LC_MONETARY = "nl_NL.UTF-8";
+		LC_NAME = "nl_NL.UTF-8";
+		LC_NUMERIC = "nl_NL.UTF-8";
+		LC_PAPER = "nl_NL.UTF-8";
+		LC_TELEPHONE = "nl_NL.UTF-8";
+		LC_TIME = "nl_NL.UTF-8";
+	};
+
+# DE config
+	services.displayManager.sddm.enable = true;
+	services.desktopManager.plasma6.enable = true;
+	services.xserver.xkb = {
+		layout = "us";
+		variant = "";
+	};
+	services.pulseaudio.enable = false;
+	security.rtkit.enable = true;
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+	};
+
+# nix configs
+
+	nixpkgs.config.allowUnfree = true;
+	nix.settings.experimental-features = [
+		"nix-command"
+		"flakes"
+	];
+	system.stateVersion = "25.05"
+
+# packages
+	programs.steam.enable = true;
+	programs.zsh.enable = true;
+	environment.systemPackages = with pkgs; [
+		# general stuff
+		neovim
+		git
+		wget
+		ghostty
+		qutebrowser
+		unzip
+		python3
+
+		# C stuff
+		gcc
+		clang
+		gdb
+		gnumake
+	];
---
+++ nixos/the-goblin.nix
@@ -0,0 +1,27 @@
+{ lib, config, pkgs, ... }:
+
+{
+	users.users.the-goblin = {
+		shell = pkgs.zsh;
+		isNormalUser = true;
+		description = "Mats de Beer";
+		extraGroups = [ "networkmanager" "wheel" ];
+		packages = with pkgs; [
+			# day-to-day
+			headsetcontrol
+
+			# Workflow
+			zsh
+			tmux
+			gh
+			fzf
+			lazygit
+			norminette
+
+			# messaging
+			discord
+			slack
+
+			# leisure
+			prismlauncher
+		]
