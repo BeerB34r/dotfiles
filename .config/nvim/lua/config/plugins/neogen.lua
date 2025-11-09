b49c818f8343e1d43886c45d4fa3e162c5908517
Mats de Beer <mde-beer@student.codam.nl>
2025-11-09 23:57:56 +0100

    smaller neogen :3

--- .config/nvim/lua/config/plugins/neogen.lua
+++ .config/nvim/lua/config/plugins/neogen.lua
@@ -1,8 +1,7 @@
 return {
 	"danymat/neogen",
-	config = function(opts)
-		require('neogen').setup(opts)
-		vim.keymap.set( "n", "<leader>d", ":Neogen<CR>", { desc = "generate documentation for current cursor position" })
-	end,
 	opts = {},
+	keys = {
+		{ "<leader>d", "<cmd>Neogen<cr>", desc = "Generate documentation for current cursor position" },
+	},
 }
