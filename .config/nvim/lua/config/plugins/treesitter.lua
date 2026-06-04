7bf31885de81e44e06dd641e168d8402e8f5344d
Mats de Beer <matsdebeerk@gmail.com>
2026-06-04 21:48:11 +0200

    foo

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -2,22 +2,22 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		lazy = false,
-		branch = 'main',
+		branch = "main",
 		build = ":TSUpdate",
 		config = function(opts)
-			local ts = require('nvim-treesitter')
-			local ensure_installed = { 'c', 'cpp', 'lua', 'markdown', 'markdown_inline', 'nix', 'haskell' }
+			local ts = require("nvim-treesitter")
+			local ensure_installed = { "c", "cpp", "lua", "markdown", "markdown_inline", "nix", "haskell" }
 			ts.setup(opts)
 
-			for _,ft in ipairs(ensure_installed) do
-				vim.api.nvim_create_autocmd('FileType', {
-					pattern = { ft },
-					callback = function()
-						vim.treesitter.start()
-					end,
-				})
-			end
+			-- for _,ft in ipairs(ensure_installed) do
+			-- 	vim.api.nvim_create_autocmd('FileType', {
+			-- 		pattern = { ft },
+			-- 		callback = function()
+			-- 			vim.treesitter.start()
+			-- 		end,
+			-- 	})
+			-- end
 		end,
 		opts = {},
-	}
+	},
 }
