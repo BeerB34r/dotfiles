ce8b192015e3fe37e424b4e24a77a656386a3eff
Mats de Beer <mde-beer@student.codam.nl>
2026-09-11 14:00:00 +0200

    style: snippets
    Signed-off-by: Mats de Beer <mde-beer@student.codam.nl>

--- .config/nvim/lua/snippets/cpp.lua
+++ .config/nvim/lua/snippets/cpp.lua
@@ -25,44 +25,41 @@ return {
 			return res
 		end)
 	),
-	s(
-		{ trig = "ocf body", snippetType = "snippet", desc = "boilerplate for orthodox canonical form" },
-		{
-			f(function(args, snip)
-				local res, env = {}, snip.env
-				local guard = env.TM_FILENAME:upper():gsub("%.", "_")
-				local class = env.TM_FILENAME:gsub("^%l", string.upper):gsub("%..*$", "")
-				local nl = function(str)
-					table.insert(res, str)
-				end
-				nl("#include <iostream>")
-				nl("")
-				nl('#include "' .. class .. '.hpp"')
-				nl("")
-				nl(class .. "::" .. class .. "() {")
-				nl('	std::clog << "default ' .. class .. ' constructor called" << std::endl;')
-				nl("}")
-				nl("")
-				nl(class .. "::" .. class .. "(const " .. class .. "& other) {")
-				nl('	std::clog << "copy ' .. class .. ' constructor called" << std::endl;')
-				nl("")
-				nl("	*this = other;")
-				nl("}")
-				nl("")
-				nl(class .. "&	" .. class .. "::operator =(const " .. class .. "& other) {")
-				nl('	std::clog << "copy ' .. class .. ' assignment called" << std::endl;')
-				nl("")
-				nl("	if (this == &other) return *this;")
-				nl("	return *this;")
-				nl("}")
-				nl("")
-				nl(class .. "::~" .. class .. "() {")
-				nl('	std::clog << "default ' .. class .. ' destructor called" << std::endl;')
-				nl("}")
-				return res
-			end),
-		}
-	),
+	s({ trig = "ocf body", snippetType = "snippet", desc = "boilerplate for orthodox canonical form" }, {
+		f(function(args, snip)
+			local res, env = {}, snip.env
+			local guard = env.TM_FILENAME:upper():gsub("%.", "_")
+			local class = env.TM_FILENAME:gsub("^%l", string.upper):gsub("%..*$", "")
+			local nl = function(str)
+				table.insert(res, str)
+			end
+			nl("#include <iostream>")
+			nl("")
+			nl('#include "' .. class .. '.hpp"')
+			nl("")
+			nl(class .. "::" .. class .. "() {")
+			nl('	std::clog << "default ' .. class .. ' constructor called" << std::endl;')
+			nl("}")
+			nl("")
+			nl(class .. "::" .. class .. "(const " .. class .. "& other) {")
+			nl('	std::clog << "copy ' .. class .. ' constructor called" << std::endl;')
+			nl("")
+			nl("	*this = other;")
+			nl("}")
+			nl("")
+			nl(class .. "&	" .. class .. "::operator =(const " .. class .. "& other) {")
+			nl('	std::clog << "copy ' .. class .. ' assignment called" << std::endl;')
+			nl("")
+			nl("	if (this == &other) return *this;")
+			nl("	return *this;")
+			nl("}")
+			nl("")
+			nl(class .. "::~" .. class .. "() {")
+			nl('	std::clog << "default ' .. class .. ' destructor called" << std::endl;')
+			nl("}")
+			return res
+		end),
+	}),
 	s({ trig = "main", snippetType = "snippet", desc = "create a standard main" }, {
 		t({
 			"#include <iostream>",
